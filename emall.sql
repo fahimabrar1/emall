3634949274a4f6125fff6d1a2ddd404c4f9b4d81
Rexaas <70713994+Rexaas@users.noreply.github.com>
2021-08-12 11:56:00 +0600

    updated order_attribute,child_catagory,

--- emall.sql
+++ emall.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 11, 2021 at 12:34 AM
+-- Generation Time: Aug 12, 2021 at 07:49 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -36,6 +36,7 @@ CREATE TABLE `admin` (
   `last_name` varchar(15) NOT NULL,
   `number` int(11) NOT NULL,
   `password` varchar(16) NOT NULL,
+  `type` varchar(10) NOT NULL,
   `shop_id_fk` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -54,12 +55,24 @@ CREATE TABLE `cart` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `catagory`
+-- Table structure for table `category`
 --
 
-CREATE TABLE `catagory` (
-  `catagory_id` varchar(10) NOT NULL,
+CREATE TABLE `category` (
+  `category_id` varchar(10) NOT NULL,
   `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `child_category`
+--
+
+CREATE TABLE `child_category` (
+  `child_cat_id` varchar(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `sub_cat_id_fk` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -90,6 +103,20 @@ CREATE TABLE `order` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `order_attribute`
+--
+
+CREATE TABLE `order_attribute` (
+  `date` date NOT NULL,
+  `product_id_fk` varchar(10) NOT NULL,
+  `shop_id_fk` varchar(10) NOT NULL,
+  `cart_id_fk` varchar(10) NOT NULL,
+  `order_id_fk` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
@@ -100,11 +127,10 @@ CREATE TABLE `product` (
   `price` int(11) NOT NULL,
   `product_category` varchar(10) NOT NULL,
   `image1` blob NOT NULL,
-  `mage2` blob NOT NULL,
+  `image2` blob NOT NULL,
   `image3` blob NOT NULL,
   `discount` int(11) NOT NULL,
-  `shop_id_fk` varchar(10) NOT NULL,
-  `sub_cat_id_fk` varchar(10) NOT NULL
+  `shop_id_fk` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -117,6 +143,7 @@ CREATE TABLE `product_attribute` (
   `product_id_fk` varchar(10) NOT NULL,
   `color_id_fk` varchar(10) NOT NULL,
   `size_id_fk` varchar(10) NOT NULL,
+  `child_cat_id_fk` varchar(10) NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -147,12 +174,12 @@ CREATE TABLE `size` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sub_catagory`
+-- Table structure for table `sub_category`
 --
 
-CREATE TABLE `sub_catagory` (
+CREATE TABLE `sub_category` (
   `sub_cat_id` varchar(10) NOT NULL,
-  `catagory_id_fk` varchar(10) NOT NULL,
+  `category_id_fk` varchar(10) NOT NULL,
   `name` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -190,10 +217,17 @@ ALTER TABLE `cart`
   ADD KEY `user_email_fk` (`user_email_fk`);
 
 --
--- Indexes for table `catagory`
+-- Indexes for table `category`
 --
-ALTER TABLE `catagory`
-  ADD PRIMARY KEY (`catagory_id`);
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `child_category`
+--
+ALTER TABLE `child_category`
+  ADD PRIMARY KEY (`child_cat_id`),
+  ADD KEY `sub_cat_id_fk` (`sub_cat_id_fk`);
 
 --
 -- Indexes for table `color`
@@ -209,12 +243,20 @@ ALTER TABLE `order`
   ADD KEY `user_email_fk` (`user_email_fk`);
 
 --
+-- Indexes for table `order_attribute`
+--
+ALTER TABLE `order_attribute`
+  ADD KEY `cart_id_fk` (`cart_id_fk`),
+  ADD KEY `order_id_fk` (`order_id_fk`),
+  ADD KEY `product_id_fk` (`product_id_fk`),
+  ADD KEY `shop_id_fk` (`shop_id_fk`);
+
+--
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`product_id`),
-  ADD KEY `shop_id_fk` (`shop_id_fk`),
-  ADD KEY `sub_cat_id` (`sub_cat_id_fk`);
+  ADD KEY `shop_id_fk` (`shop_id_fk`);
 
 --
 -- Indexes for table `product_attribute`
@@ -222,7 +264,8 @@ ALTER TABLE `product`
 ALTER TABLE `product_attribute`
   ADD KEY `product_attribute_ibfk_1` (`color_id_fk`),
   ADD KEY `size_id_fk` (`size_id_fk`),
-  ADD KEY `product_id_fk` (`product_id_fk`);
+  ADD KEY `product_id_fk` (`product_id_fk`),
+  ADD KEY `child_cat_id_fk` (`child_cat_id_fk`);
 
 --
 -- Indexes for table `shop`
@@ -238,11 +281,11 @@ ALTER TABLE `size`
   ADD PRIMARY KEY (`size_id`);
 
 --
--- Indexes for table `sub_catagory`
+-- Indexes for table `sub_category`
 --
-ALTER TABLE `sub_catagory`
+ALTER TABLE `sub_category`
   ADD PRIMARY KEY (`sub_cat_id`),
-  ADD KEY `catagory_id_fk` (`catagory_id_fk`);
+  ADD KEY `catagory_id_fk` (`category_id_fk`);
 
 --
 -- Indexes for table `user`
@@ -267,17 +310,31 @@ ALTER TABLE `cart`
   ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_email_fk`) REFERENCES `user` (`email`);
 
 --
+-- Constraints for table `child_category`
+--
+ALTER TABLE `child_category`
+  ADD CONSTRAINT `child_category_ibfk_1` FOREIGN KEY (`sub_cat_id_fk`) REFERENCES `sub_category` (`sub_cat_id`);
+
+--
 -- Constraints for table `order`
 --
 ALTER TABLE `order`
   ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`user_email_fk`) REFERENCES `user` (`email`);
 
 --
+-- Constraints for table `order_attribute`
+--
+ALTER TABLE `order_attribute`
+  ADD CONSTRAINT `order_attribute_ibfk_1` FOREIGN KEY (`cart_id_fk`) REFERENCES `cart` (`cart_id`),
+  ADD CONSTRAINT `order_attribute_ibfk_2` FOREIGN KEY (`order_id_fk`) REFERENCES `order` (`order_id`),
+  ADD CONSTRAINT `order_attribute_ibfk_3` FOREIGN KEY (`product_id_fk`) REFERENCES `product` (`product_id`),
+  ADD CONSTRAINT `order_attribute_ibfk_4` FOREIGN KEY (`shop_id_fk`) REFERENCES `shop` (`shop_id`);
+
+--
 -- Constraints for table `product`
 --
 ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`shop_id_fk`) REFERENCES `shop` (`shop_id`),
-  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`sub_cat_id_fk`) REFERENCES `sub_catagory` (`sub_cat_id`);
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`shop_id_fk`) REFERENCES `shop` (`shop_id`);
 
 --
 -- Constraints for table `product_attribute`
@@ -285,19 +342,20 @@ ALTER TABLE `product`
 ALTER TABLE `product_attribute`
   ADD CONSTRAINT `product_attribute_ibfk_1` FOREIGN KEY (`color_id_fk`) REFERENCES `color` (`color_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `product_attribute_ibfk_2` FOREIGN KEY (`size_id_fk`) REFERENCES `size` (`size_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `product_attribute_ibfk_3` FOREIGN KEY (`product_id_fk`) REFERENCES `product` (`product_id`);
+  ADD CONSTRAINT `product_attribute_ibfk_3` FOREIGN KEY (`product_id_fk`) REFERENCES `product` (`product_id`),
+  ADD CONSTRAINT `product_attribute_ibfk_4` FOREIGN KEY (`child_cat_id_fk`) REFERENCES `child_category` (`child_cat_id`);
 
 --
 -- Constraints for table `shop`
 --
 ALTER TABLE `shop`
-  ADD CONSTRAINT `shop_ibfk_1` FOREIGN KEY (`category_id_fk`) REFERENCES `catagory` (`catagory_id`);
+  ADD CONSTRAINT `shop_ibfk_1` FOREIGN KEY (`category_id_fk`) REFERENCES `category` (`category_id`);
 
 --
--- Constraints for table `sub_catagory`
+-- Constraints for table `sub_category`
 --
-ALTER TABLE `sub_catagory`
-  ADD CONSTRAINT `sub_catagory_ibfk_1` FOREIGN KEY (`catagory_id_fk`) REFERENCES `catagory` (`catagory_id`);
+ALTER TABLE `sub_category`
+  ADD CONSTRAINT `sub_category_ibfk_1` FOREIGN KEY (`category_id_fk`) REFERENCES `category` (`category_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
